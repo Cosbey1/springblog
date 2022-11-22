99850bac795ce8288d3b5a250ead2a7899e5df52
CleoBreaux <corey.osbey@gmail.com>
2022-11-22 14:44:30 -0600

    dice game exercise

--- src/main/java/com/example/springblog/MathController.java
+++ src/main/java/com/example/springblog/MathController.java
@@ -3,6 +3,7 @@ package com.example.springblog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -30,5 +31,32 @@ public class MathController {
     @ResponseBody
     public int divide(@PathVariable int number1, @PathVariable int number2 ) {
         return number1 / number2;
+    }
+
+    // TODO: Create a get mapping to /roll-dice to show anchor tag options
+
+    @GetMapping("/roll-dice")
+    public String choices(){
+        return "number";
+    }
+
+    @GetMapping("/roll-dice/{num}")
+    // Use Path Variable
+    public String guessNumber(@PathVariable int num) {
+
+        // TODO: Create a variable that will store a random number between 1-6
+        int guessNumber = (int)Math.floor(Math.random()*6+1);
+
+        // TODO: Create a conditinal statement to check if the path variable and the random number match.
+        if(num == guessNumber){
+            return "rightGuess";
+        }else{
+            if ( num != guessNumber  )
+            return "wrongGuess";
+        }
+        // TODO: Finally, pass a boolean to the number template to inform the user if they were correct or not.
+
+//
+        return null;
     }
 }
